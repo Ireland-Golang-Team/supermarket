34544b8729d94632f3226026c6eb31d6709b4ce5
jtc23 <936865249@qq.com>
2020-11-13 15:15:39 +0800

    rewrite some logic in the project

--- main.go
+++ main.go
@@ -10,9 +10,12 @@ import (
 	//  "strconv"
 )
 var lostnum int = 0
+var finishnum int = 0
+var totalWait float64 = 0
 var  numCashier int
 var  numLessCashier int
 var globalmutex sync.Mutex
+var globalmutexwaittime sync.Mutex
 var consumers []*Consumer
 
 type Cashier struct {
@@ -26,6 +29,7 @@ type Consumer struct {
 	ProductNumber int
 	Checkspeed int
 	WaitTime int
+	BeginTime int64
 }
 // func mutiply(a float, b int) (int c){
 // 	c := int64(decimal.NewFromFloat(num1).Mul(decimal.NewFromFloat(float64(num2))))
@@ -41,14 +45,28 @@ func (c *Cashier) addConsumer(inVal int) {
 }
 
 
-func (c Cashier) doCheckout() {
+func (c Cashier) doCheckout(wg *sync.WaitGroup) {
 		for{
-			time.Sleep(time.Millisecond*50)
+			// time.Sleep(time.Millisecond*50)
 			if len(c.WaitGroup)>0{
 				consumerID := <- c.WaitGroup
-				// fmt.Println(consumers[consumerID].Checkspeed)
-				time.Sleep(time.Millisecond * time.Duration(consumers[consumerID].Checkspeed*consumers[consumerID].ProductNumber))
-				fmt.Printf("checkout compelete consumer %d cashier %d item %d\n",consumerID,c.Id,consumers[consumerID].ProductNumber)
+				 var cstZone = time.FixedZone("CST", 8*3600)
+				// loc, _ := time.LoadLocation("Local")
+				TimeNow:=time.Now().In(cstZone).UnixNano()
+				left:=float64(TimeNow-consumers[consumerID].BeginTime)
+				waitTime:=left/1e9
+				globalmutexwaittime.Lock()
+				totalWait=totalWait+waitTime
+				globalmutexwaittime.Unlock()
+				// begintime:=time.Unix(consumers[consumerID].BeginTime, 0).Format("2006-01-02 15:04:05" )
+				// TimeBegin, _ := time.ParseInLocation("2006-01-02 15:04:05",begintime,loc)
+				// left := TimeNow.Sub(TimeBegin)
+				time.Sleep(time.Second * time.Duration(consumers[consumerID].Checkspeed*consumers[consumerID].ProductNumber))
+				globalmutexwaittime.Lock()
+				finishnum++
+				fmt.Printf("checkout compelete consumer %d cashier %d item %d wait time %f totalwait %f finshpeople %d\n",consumerID,c.Id,consumers[consumerID].ProductNumber,waitTime,totalWait,finishnum)
+				globalmutexwaittime.Unlock()
+				wg.Done()
 			}
 		}
 }
@@ -56,6 +74,12 @@ func (c *Consumer) setWaitTime() {
 	num :=rand.Intn(60)
 	c.WaitTime=num
 }
+func (c *Consumer) setBeginTime() {
+	var cstZone = time.FixedZone("CST", 8*3600)
+	time:=time.Now().In(cstZone).UnixNano()
+	c.BeginTime=time
+}
+
 func (c *Consumer) setProductNumber() {
 	num :=rand.Float64();
 	// fmt.Println(num)
@@ -69,14 +93,29 @@ func (c *Consumer) setProductNumber() {
 	}
 }
 func (c *Consumer) setCheckspeed() {
-	c.Checkspeed  =rand.Intn(5) + 1
+	num:=rand.Intn(100)
+	if num>=99{
+		c.Checkspeed=6
+	}else if num>=95{
+		c.Checkspeed=5
+	}else if num >=90{
+		c.Checkspeed=4
+	}else if num>= 85{
+		c.Checkspeed=3
+	}else if num>=80{
+		c.Checkspeed=2
+	}else if num>=70{
+		c.Checkspeed=1
+	}else{
+		c.Checkspeed=1
+	}
+	// c.Checkspeed  =rand.Intn(5) + 1
 	// c.Checkspeed = rand.Float64()*5.5+0.5
 }
 func (c *Consumer) findQueue(cashiers []*Cashier,wg *sync.WaitGroup){
-	defer wg.Done()
 	var num int = -1;
 	var cap int = 6 ;
-	time.Sleep(time.Millisecond *10* time.Duration(c.WaitTime))
+	time.Sleep(time.Second * time.Duration(c.WaitTime))
 	if(c.ProductNumber<=5){
 		for i := 0; i < numLessCashier; i++ {
 			cashiers[i].lock.Lock()
@@ -105,6 +144,7 @@ func (c *Consumer) findQueue(cashiers []*Cashier,wg *sync.WaitGroup){
 		lostnum++
 		fmt.Printf("consumer %d leave total:%d\n",c.Id,lostnum)
 		globalmutex.Unlock()
+		defer wg.Done()
 	}else{
 		cashiers[num].lock.Lock()
 		cashiers[num].addConsumer(c.Id)
@@ -131,12 +171,14 @@ func (conf *Consumer) String() string {
 }
 
 func main()  {
+	fmt.Println(time.Now())
 	fmt.Println("please int number of cashier between 1 and 8：")
 	fmt.Scanln(&numCashier)
 	fmt.Println("please int number of restriction cashier between 1 and 2:")
 	fmt.Scanln(&numLessCashier)
 	cashiers := make([]*Cashier, numCashier)
-	var wgcashier sync.WaitGroup
+	// var wgcashier sync.WaitGroup
+	var wg sync.WaitGroup
 	for i := 0; i < numCashier; i++ {
 		cashiers[i] = &Cashier{
 			Id: i,WaitGroup:make(chan int,6)}
@@ -145,8 +187,8 @@ func main()  {
 			}else{
 				cashiers[i].setLimit(200)
 			}
-			wgcashier.Add(1)
-			go cashiers[i].doCheckout()
+			// wgcashier.Add(1)
+			go cashiers[i].doCheckout(&wg)
 	}
 
 	// for i := 0; i < numCashier; i++ {
@@ -157,20 +199,21 @@ func main()  {
 	// for i := 0; i < numCashier; i++ {
 	// 	fmt.Println(<-locks[i])
 	// }
-	var wg sync.WaitGroup
 	for i := 0; i < 1000; i++ {
-		time.Sleep(time.Millisecond *10)
+		num :=rand.Intn(5)+1
+		
+		time.Sleep(time.Millisecond *time.Duration(num))
 		consumers = append(consumers,&Consumer{
 			Id: i})
 			consumers[i].setProductNumber() 
 			consumers[i].setCheckspeed()
 			consumers[i].setWaitTime()
+			consumers[i].setBeginTime()
 			wg.Add(1)
 			go consumers[i].findQueue(cashiers,&wg)
 
 	}
 	wg.Wait()
-	wgcashier.Wait()
 	// for i := 0; i < 10; i++ {
 	// 	fmt.Println(consumers[i].String())
 	// }
---
+++ test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	// "sync"
+	// "time"
+	// "bytes"
+	//  "encoding/json"
+	//  "math/rand"
+	//  "strconv"
+)
+func main{
+	a:="1.22s"
+	printf(int64(a))
+}
